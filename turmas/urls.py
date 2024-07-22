b38fc6a05d07615b1602df5b200c0912349a6a18
Romeu Borges <romeuborges19@gmail.com>
2024-07-22 18:11:49 -0300

    fix: ajustando rota de login

--- turmas/urls.py
+++ turmas/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from turmas.views import (
     TurmasCreateView,
@@ -13,19 +14,19 @@ from turmas.views import (
 app_name = "turmas"
 
 urlpatterns = [
-    path("", TurmasIndexView.as_view(), name="index"),
-    path("turmas/criar", TurmasCreateView.as_view(), name="criar"),
-    path("turmas/<int:pk>", TurmaDetailView.as_view(), name="ver"),
-    path("turmas/<int:pk>/editar", TurmasUpdateView.as_view(), name="editar"),
-    path("turmas/<int:pk>/aluno/criar", AlunoCreateView.as_view(), name="aluno_criar"),
+    path("", login_required(TurmasIndexView.as_view()), name="index"),
+    path("turmas/criar", login_required(TurmasCreateView.as_view()), name="criar"),
+    path("turmas/<int:pk>",  login_required(TurmaDetailView.as_view()), name="ver"),
+    path("turmas/<int:pk>/editar",  login_required(TurmasUpdateView.as_view()), name="editar"),
+    path("turmas/<int:pk>/aluno/criar",  login_required(AlunoCreateView.as_view()), name="aluno_criar"),
     path(
         "turmas/<int:pk_turma>/aluno/<int:pk_aluno>/editar",
-        AlunoUpdateView.as_view(),
+        login_required(AlunoUpdateView.as_view()),
         name="aluno_editar",
     ),
     path(
         "turmas/<int:pk>/container/start",
-        ContainerStartView.as_view(),
+        login_required(ContainerStartView.as_view()),
         name="subir_container",
     ),
 ]
